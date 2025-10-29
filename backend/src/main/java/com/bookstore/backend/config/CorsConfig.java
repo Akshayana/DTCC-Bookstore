db8357195d60893ff928b0e4dc1e9d59420ff59a
Akshayana <125847491+Akshayana@users.noreply.github.com>
2025-10-29 14:23:10 +0530

    Update CorsConfig.java

--- backend/src/main/java/com/bookstore/backend/config/CorsConfig.java
+++ backend/src/main/java/com/bookstore/backend/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig implements  WebMvcConfigurer {
                         .allowedOrigins("*")  
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                         .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowCredentials(false);
             
     }
 }
