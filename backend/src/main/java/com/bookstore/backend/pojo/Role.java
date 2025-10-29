0c74c604f7a9f8939267b6553ac54ec500b3c36c
Akshayana <125847491+Akshayana@users.noreply.github.com>
2025-10-29 18:18:11 +0530

    Update Role.java

--- backend/src/main/java/com/bookstore/backend/pojo/Role.java
+++ backend/src/main/java/com/bookstore/backend/pojo/Role.java
@@ -11,7 +11,7 @@ import lombok.*;
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
     @Column(nullable = false, unique = true)
     private String name; 
