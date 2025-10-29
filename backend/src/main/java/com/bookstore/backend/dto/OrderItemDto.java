8e9d055b69ac51a19feaf0c987226b8e16bf947b
Akshayana <125847491+Akshayana@users.noreply.github.com>
2025-10-29 18:22:28 +0530

    Update OrderItemDto.java

--- backend/src/main/java/com/bookstore/backend/dto/OrderItemDto.java
+++ backend/src/main/java/com/bookstore/backend/dto/OrderItemDto.java
@@ -9,6 +9,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 
 public class OrderItemDto {         
-    private Integer bookId;       
+    private Long bookId;       
     private Integer quantity;     
 }
